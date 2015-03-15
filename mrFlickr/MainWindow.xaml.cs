78b528fb9a1266e979c4a87531d80957eb00cedc
slashvlz <netupskiy@gmail.com>
2015-03-15 22:44:33 +0500

    test1

--- mrFlickr/MainWindow.xaml.cs
+++ mrFlickr/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using FlickrNet;
 
 namespace mrFlickr
 {
@@ -20,9 +21,58 @@ namespace mrFlickr
     /// </summary>
     public partial class MainWindow : Window
     {
+        private OAuthRequestToken requestToken;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Flickr f = fAuth.GetInstance();
+            requestToken = f.OAuthGetRequestToken("oob");
+
+            string url = f.OAuthCalculateAuthorizationUrl(requestToken.Token, AuthLevel.Write);
+
+            System.Diagnostics.Process.Start(url);
+
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+           /* if (String.IsNullOrEmpty(verify_app.Text))
+            {
+                MessageBox.Show("Нужно вставить проверочный код.");
+                return;
+            }*/
+
+            Flickr f = fAuth.GetAuthInstance();
+
+            GalleryCollection gal_col= f.GalleriesGetList();
+
+            foreach (Gallery gal in gal_col)
+            {
+
+            }
+            
+
+            try
+            {
+                string token = "72157649041023933-db370f453929fe76";
+                string tokenSecret = "eca4d08ecf331ff5";
+                
+                string verifier_text="309-033-020"; //verify_app.Text;
+                var accessToken = f.OAuthGetAccessToken(token, tokenSecret, verifier_text);
+                fAuth.OAuthToken = accessToken;
+                FlickrNet.OAuthAccessToken ftoken = new OAuthAccessToken();
+                
+                this.Title = "Авторизован как " + accessToken.FullName;
+            }
+            catch (FlickrApiException ex)
+            {
+                MessageBox.Show("Ошибка авторизации: " + ex.Message);
+            }
         }
     }
 }
---
+++ mrFlickr/fAuth.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlickrNet;
+
+namespace mrFlickr
+{
+    class fAuth
+    {
+
+        public const string ApiKey = "6955460ccf9ada544467db10027c443e";
+        public const string SharedSecret = "cfafac5e2caca114";
+
+        public static Flickr GetInstance()
+        {
+            return new Flickr(ApiKey, SharedSecret);
+        }
+
+        public static Flickr GetAuthInstance()
+        {
+            var f = new Flickr(ApiKey, SharedSecret);
+            f.OAuthAccessToken = OAuthToken.Token;
+            f.OAuthAccessTokenSecret = OAuthToken.TokenSecret;
+            return f;
+        }
+
+        public static OAuthAccessToken OAuthToken
+        {
+            get
+            {
+                return Properties.Settings.Default.fOAuthToken;
+            }
+            set
+            {
+                Properties.Settings.Default.fOAuthToken = value;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+
+    }
+}
